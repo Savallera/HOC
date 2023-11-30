e451b65dd2c6bd0c368b76424bb9527aeb0eb51b
Savallera <phenixvn1985@gmail.com>
2023-11-30 17:39:06 +0300

    add menu mobile script

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,19 +1,3 @@
-
-const headerJoinButton = document.querySelector('.header__join-button');
-const feebackModal = document.querySelector('.feedback-modal');
-const feebackModalCloseButton = document.querySelector(
-  '.feedback-modal__close-button'
-);
-const openFeedbackModal = function () {
-  feebackModal.classList.add('feedback-modal--opened');
-};
-const closeFeedbackModal = function () {
-  feebackModal.classList.remove('feedback-modal--opened');
-};
-
-headerJoinButton.addEventListener('click', openFeedbackModal);
-feebackModalCloseButton.addEventListener('click', closeFeedbackModal);
-
 // Header menu for mobile
 const menuList = document.querySelector('.header__menu-list');
 const mobileMenu = document.querySelector('.header__drop-down-menu');
