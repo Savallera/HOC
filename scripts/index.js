df65c6d45fac7a5d8822708935a2a434f1e1559f
Savallera <phenixvn1985@gmail.com>
2023-11-30 17:39:19 +0300

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,57 @@ const closeFeedbackModal = function () {
 
 headerJoinButton.addEventListener('click', openFeedbackModal);
 feebackModalCloseButton.addEventListener('click', closeFeedbackModal);
+
+// Header menu for mobile
+const menuList = document.querySelector('.header__menu-list');
+const mobileMenu = document.querySelector('.header__drop-down-menu');
+const mobileMenuList = document.querySelector('.header__drop-down-menu-list');
+
+let numberOfItems = 0;
+let totalSpace = 0;
+let breakWidths = [];
+let availableSpace, numOfVisibleItems, requiredSpace;
+Array.from(menuList.children).map((children) => {
+  totalSpace += children.getBoundingClientRect().width;
+  numberOfItems += 1;
+  breakWidths.push(totalSpace);
+});
+
+function checkMenuSize() {
+  // Get instant state
+  availableSpace = menuList.getBoundingClientRect().width;
+  numOfVisibleItems = menuList.children.length;
+  requiredSpace = breakWidths[numOfVisibleItems - 1];
+
+  // There is not enought space
+  if (requiredSpace > availableSpace) {
+    mobileMenuList.prepend(menuList.children[menuList.children.length - 1]);
+    mobileMenuList.firstElementChild
+      .querySelector('a')
+      .classList.remove('header__menu-link');
+    mobileMenuList.firstElementChild
+      .querySelector('a')
+      .classList.add('header__drop-down-menu-link');
+    numOfVisibleItems -= 1;
+    checkMenuSize();
+    // There is more than enough space
+  } else if (availableSpace > breakWidths[numOfVisibleItems]) {
+    menuList.append(mobileMenuList.children[0]);
+    menuList.lastElementChild
+      .querySelector('a')
+      .classList.remove('header__drop-down-menu-link');
+    menuList.lastElementChild
+      .querySelector('a')
+      .classList.add('header__menu-link');
+    numOfVisibleItems += 1;
+  }
+}
+
+window.addEventListener('resize', () => {
+  checkMenuSize();
+});
+window.addEventListener('DOMContentLoaded', () => {
+  checkMenuSize();
+
+  console.log(requiredSpace, availableSpace);
+});
