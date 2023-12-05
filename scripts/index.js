17303aa2932518d20187dacc0e957dd2c04a8229
Savallera <phenixvn1985@gmail.com>
2023-12-05 18:47:51 +0300

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -207,6 +207,27 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+// Форма присоединиться к проекту
+const signUpButton = document.querySelector('.sign-up__button');
+const signUpModal = document.querySelector('.modal--content-sign-up');
+const signUpModalCloseButton = document.querySelector('.modal__close-button');
+const openSignUpModal = function () {
+  signUpModal.classList.add('modal--opened');
+};
+const closeSignUpModal = function () {
+  signUpModal.classList.remove('modal--opened');
+};
+
+signUpButton.addEventListener('click', openSignUpModal);
+signUpModal.addEventListener('click', (evt) => {
+  if (
+    evt.target.classList.contains('modal--content-sign-up') ||
+    evt.target.classList.contains('modal__close-button')
+  ) {
+    closeSignUpModal();
+  }
+});
+
 window.addEventListener('DOMContentLoaded', () => {
   if (Splide) {
     // if (document.querySelector('#article-slider')) {
@@ -248,27 +269,6 @@ window.addEventListener('DOMContentLoaded', () => {
     //   });
     //   festivalSlider.mount();
     // }
-  }
-});
-
-// Форма присоединиться к проекту
-const signUpButton = document.querySelector('.sign-up__button');
-const signUpModal = document.querySelector('.modal--content-sign-up');
-const signUpModalCloseButton = document.querySelector('.modal__close-button');
-const openSignUpModal = function () {
-  signUpModal.classList.add('modal--opened');
-};
-const closeSignUpModal = function () {
-  signUpModal.classList.remove('modal--opened');
-};
-
-signUpButton.addEventListener('click', openSignUpModal);
-signUpModal.addEventListener('click', (evt) => {
-  if (
-    evt.target.classList.contains('modal--content-sign-up') ||
-    evt.target.classList.contains('modal__close-button')
-  ) {
-    closeSignUpModal();
   }
 });
 
