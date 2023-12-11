88eb4d9b8f387d6d2bfdb0738cc7d6d2224636d1
Savallera <phenixvn1985@gmail.com>
2023-12-11 17:04:19 +0300

    страница студСМИ + форма регистрации

---
+++ src/blocks/stud-smi/stud-smi.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', () => {
+  if (Splide) {
+    // Слайдер карточек на странице студСМИ
+    if (document.querySelector('#stud-smi-cards-slider')) {
+      let studSmiCardsSlider = new Splide('#stud-smi-cards-slider', {
+        type: 'slide',
+        autoplay: false,
+        arrows: false,
+        pagination: false,
+        gap: '.9375em',
+        autoWidth: true,
+        padding: 'var(--section-padding)',
+        mediaQuery: 'min',
+        breakpoints: {
+          768: {
+            destroy: 'completely',
+          },
+        },
+      });
+      studSmiCardsSlider.mount();
+    }
+
+    // Слайдер новостей на странице студСМИ
+    if (document.querySelector('#stud-smi-news-slider')) {
+      let studSmiNewsSlider = new Splide('#stud-smi-news-slider', {
+        type: 'fade',
+        rewind: true,
+        autoplay: true,
+        arrows: false,
+      });
+      studSmiNewsSlider.mount();
+    }
+  }
+});
