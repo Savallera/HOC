471146a93a2fec29803e6304401a0acd551fa731
Savallera <phenixvn1985@gmail.com>
2023-12-11 13:42:31 +0300

    Правила публикации

--- src/blocks/accordion/accordion.js
+++ src/blocks/accordion/accordion.js
@@ -1,24 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
-
   // Аккордеоны
   if (document.querySelector('.accordion')) {
-    let accordionsButtons =
+    const accordionsButtons =
       document.getElementsByClassName('accordion__button');
-    let i;
 
     for (i = 0; i < accordionsButtons.length; i++) {
       accordionsButtons[i].addEventListener('click', function () {
         this.closest('.accordion').classList.toggle('accordion--actived');
         let icon = this.querySelector('.accordion__icon');
-        let panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
+        let container = this.nextElementSibling;
+        if (container.style.maxHeight) {
           this.setAttribute('aria-label', 'Развернуть');
-          panel.style.maxHeight = null;
+          container.style.maxHeight = null;
           icon.classList.add('icon--type-plus');
           icon.classList.remove('icon--type-minus');
         } else {
           this.setAttribute('aria-label', 'Свернуть');
-          panel.style.maxHeight = panel.scrollHeight + 'px';
+          container.style.maxHeight = container.scrollHeight + 'px';
           icon.classList.add('icon--type-minus');
           icon.classList.remove('icon--type-plus');
         }
