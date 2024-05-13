0883c081887e3fc84aa488cf0033e0db7c77d1ea
Savallera <phenix-vn@yandex.ru>
2024-05-13 19:06:44 +0300

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -695,35 +695,6 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-  // Квиз
-  if (document.querySelector('.survey__form')) {
-    const surveyLabels = document.querySelectorAll('.survey__label');
-
-    for (i = 0; i < surveyLabels.length; i++) {
-      surveyLabels[i].addEventListener('click', function () {
-        this.classList.add('survey__label--current');
-        this.closest('.survey__form').classList.add(
-          'survey__form--show-result'
-        );
-      });
-    }
-  }
-
-  if (Splide) {
-    // Слайдер с квизами на главной странице
-    if (document.querySelector('#survey-slider')) {
-      let eventsSlider = new Splide('#survey-slider', {
-        rewind: true,
-        autoplay: true,
-        arrows: false,
-        gap: 'var(--gap-slider)',
-      });
-      eventsSlider.mount();
-    }
-  }
-});
-
-window.addEventListener('DOMContentLoaded', () => {
   // Вкладки
   if (document.querySelector('.tabs')) {
     const tabContainer = document.querySelector('.tabs');
@@ -753,6 +724,35 @@ window.addEventListener('DOMContentLoaded', () => {
         openTabSection(button);
       })
     );
+  }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Квиз
+  if (document.querySelector('.survey__form')) {
+    const surveyLabels = document.querySelectorAll('.survey__label');
+
+    for (i = 0; i < surveyLabels.length; i++) {
+      surveyLabels[i].addEventListener('click', function () {
+        this.classList.add('survey__label--current');
+        this.closest('.survey__form').classList.add(
+          'survey__form--show-result'
+        );
+      });
+    }
+  }
+
+  if (Splide) {
+    // Слайдер с квизами на главной странице
+    if (document.querySelector('#survey-slider')) {
+      let eventsSlider = new Splide('#survey-slider', {
+        rewind: true,
+        autoplay: true,
+        arrows: false,
+        gap: 'var(--gap-slider)',
+      });
+      eventsSlider.mount();
+    }
   }
 });
 
